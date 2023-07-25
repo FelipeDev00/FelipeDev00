3d798b78e9a59d6d0f065b1288494e558b0ad0fe
Felipe Rodrigues <133248921+FelipeDev00@users.noreply.github.com>
2023-07-25 16:30:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var nome = prompt('Digite seu nome para acessar a pagina:')
+while ( nome!= 'Rocky') {
+    alert('Você não é o desenvolvedor desse site.')
+    nome = prompt('Digite seu nome para acessar a pagina:')
+}
+alert('Acesso liberado, bem vindo, Felipe!')
+var fnome = prompt('Digite seu nome:')
+
+function carregar() {
+    var msg = document.getElementById('msg')
+    var data = new Date()
+    var hora = data.getHours()
+    if (hora >= 0 && hora < 12){
+        msg.innerHTML = `Bom dia, ${fnome}!`}
+        else if (hora >=12 && hora < 18){
+            msg.innerHTML = `Boa tarde, ${fnome}!`
+        }
+        else
+            {
+            msg.innerHTML = `Boa noite, ${fnome}!`
+        }
+}
